f2631d25200460c903ac29f1e87a70560c4117af
aric.zheng <1290657123@qq.com>
2025-10-24 20:55:43 +0800

    fix: rm default extra-view

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,10 +1,8 @@
 // import noop from '@jswork/noop';
 import cx from 'classnames';
 import React, { Component } from 'react';
-import { Button, Card, CardProps, Space } from 'antd';
-import { AcTable, AcTableProps } from '@jswork/antd-components';
-import { PlusOutlined, ReloadOutlined } from '@ant-design/icons';
-import nx from '@jswork/next';
+import { Card, CardProps } from 'antd';
+import { AcTable, AcTableExtras, AcTableProps } from '@jswork/antd-components';
 
 const CLASS_NAME = 'react-ant-resource-list';
 
@@ -23,17 +21,6 @@ export type ReactAntResourceListProps = CardProps & {
   tableProps?: Omit<AcTableProps, 'name' | 'columns' | 'params'>;
 };
 
-const locales = {
-  'zh-CN': {
-    refresh: '刷新',
-    add: '添加',
-  },
-  'en-US': {
-    refresh: 'Refresh',
-    add: 'Create',
-  },
-};
-
 export default class ReactAntResourceList extends Component<ReactAntResourceListProps> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
@@ -43,34 +30,6 @@ export default class ReactAntResourceList extends Component<ReactAntResourceList
     module: 'admin',
   };
 
-  private t = (key: string) => {
-    const { lang } = this.props;
-    return locales[lang!][key] || key;
-  };
-
-  get extraView() {
-    return (
-      <Space>
-        <Button size="small" icon={<ReloadOutlined />} onClick={this.handleRefresh}>
-          {this.t('refresh')}
-        </Button>
-        <Button size="small" icon={<PlusOutlined />} onClick={this.handleAdd}>
-          {this.t('add')}
-        </Button>
-      </Space>
-    );
-  }
-
-  private handleRefresh = () => {
-    const { name } = this.props;
-    nx.$event?.emit?.(`${name}:reset`);
-  };
-
-  private handleAdd = () => {
-    const { name } = this.props;
-    nx.$event?.emit?.(`${name}:add`);
-  };
-
   render() {
     const { className, module, name, tableProps, params, columns, ...rest } = this.props;
 
@@ -78,12 +37,13 @@ export default class ReactAntResourceList extends Component<ReactAntResourceList
       <Card
         data-component={CLASS_NAME}
         className={cx(CLASS_NAME, className)}
-        extra={this.extraView}
+        extra={<AcTableExtras name={name} />}
         {...rest}>
         <AcTable
           bordered
           size="middle"
           name={name}
+          module={module}
           columns={columns}
           params={params}
           {...tableProps}
