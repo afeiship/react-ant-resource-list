25d2cd916022ba7811bf2b6a4f5f6efafd13e0a4
aric.zheng <1290657123@qq.com>
2025-10-27 14:08:02 +0800

    feat: add test new antd-components

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
       <div className="badge badge-warning absolute right-0 top-0 m-4">Build Time: {BUILD_TIME}</div>
-      <ReactAntResourceList title="文章列表" name="posts" columns={columns} />
+      <ReactAntResourceList title="文章列表" name="posts" columns={columns} actions={['add']} />
     </div>
   );
 }
