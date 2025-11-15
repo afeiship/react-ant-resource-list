6dd3d148c0d9144f4e8679e92c8441a90ea11a99
aric.zheng <1290657123@qq.com>
2025-11-15 15:56:26 +0800

    fix: card extra has highest order

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -48,7 +48,7 @@ export default class ReactAntResourceList extends Component<ReactAntResourceList
     const { cardExtraProps = {}, name, lang, hasBack } = this.props;
     const defaultActions = cardExtraProps.actions || ['add', 'reset'];
     const actions = hasBack ? [...defaultActions, 'back'] : defaultActions;
-    return <AcCardExtras name={name} lang={lang} {...cardExtraProps} actions={actions} />;
+    return <AcCardExtras name={name} lang={lang} actions={actions} {...cardExtraProps} />;
   }
 
   render() {
